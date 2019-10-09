38c7c26dd87eb14e6f9b33d263d85e34dfb8ac3c
Connor Hock <cbh06002@creighton.edu>
2019-10-09 15:05:57 -0500

    final

--- forms/Calculate/Calculate.js
+++ forms/Calculate/Calculate.js
@@ -1,8 +1,3 @@
-/* Create a new form named calculate.
-Scenario: create a program that finds the average of three numbers times the square of the first number. 
-Do NOT use any controls or events. 
-
-
 let num1 = Number(prompt("Enter your first number."));
 let num2 = Number(prompt("Enter your second number."));
 let num3 = Number(prompt("Enter your second number."));
@@ -14,4 +9,3 @@ function calcAvgSquare(num1, num2, num3) {
 
 let finalAnswer = calcAvgSquare(num1, num2, num3);
 alert(`The answer using ${num1}, ${num2} and ${num3} is ${finalAnswer}.`);
-*/
